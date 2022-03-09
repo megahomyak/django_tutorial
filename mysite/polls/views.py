f86aba7125b5520df008da20afd2b71dfe83526d
megahomyak <g.megahomyak@gmail.com>
2022-03-09 20:00:06 +0500

    Now you can properly vote in polls!

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -23,5 +24,18 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    response = f"You're voting on question {question_id}."
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    # noinspection PyUnresolvedReferences
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )
