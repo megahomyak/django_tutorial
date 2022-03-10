364370c1faa58ad71396b1ef71e1d044ffc66bc5
megahomyak <g.megahomyak@gmail.com>
2022-03-10 21:16:52 +0500

    Choices are compact now!

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     # 3 extra empty choice cards by default (NOT saved to the database when
     # you click "SAVE")
