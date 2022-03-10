04a6e154dc3f41dc5cc5c48dbc85233c82f3f350
megahomyak <g.megahomyak@gmail.com>
2022-03-10 21:16:27 +0500

    Choices are now on the same page with Questions!

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,11 +3,19 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    # 3 extra empty choice cards by default (NOT saved to the database when
+    # you click "SAVE")
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
