52d27f5cb6c079f9c4f9b39a7bb87872ce0987c8
megahomyak <g.megahomyak@gmail.com>
2022-03-10 20:24:11 +0500

    Field reordering in the admins' page!

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register((Question, Choice))
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]  # Custom field order
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
